4a589fd8c87cf19676f0ffe08d1148f6e0055c65
Rover Babai <66141875+rin2de@users.noreply.github.com>
2022-08-17 00:19:51 +0530

    Drum

---
+++ index.js
@@ -0,0 +1,63 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick() {
+
+
+    var buttonInnerHtml = this.innerHTML;
+
+    makeSound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
+  });
+}
+
+
+document.addEventListener('keydown', function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+function makeSound(key) {
+  switch (key) {
+    case "a":
+      var audio = new Audio('tom-1.mp3');
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio('tom-2.mp3');
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio('tom-3.mp3');
+      audio.play();
+      break;
+    case "f":
+      var audio = new Audio('tom-4.mp3');
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio('snare.mp3');
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio('crash.mp3');
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio('kick-bass.mp3');
+      audio.play();
+      break;
+
+    default:
+      console.log(buttonInnerHtml);
+  }
+
+}
+
+function buttonAnimation(currentKey) {
+var activeButton=document.querySelector("." + currentKey);
+activeButton.classList.add("pressed");
+
+setTimeout (function() {
+  activeButton.classList.remove("pressed");
+}, 100);
+}
